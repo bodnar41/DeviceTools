56cd54e7e76e3cef511b615dddc7850c6b1c4438
bodnar41 <b.maark97@gmail.com>
2021-03-16 13:06:03 +0100

    modified SSH modul

--- SSH_CONN.py
+++ SSH_CONN.py
@@ -1,13 +1,13 @@
 import paramiko, time
 
 
-def ssh_conn(ip = None, user = None, pwd = None, cmd = None):
-    print("Creating SSH Client..")
+def ssh_conn(ip, user, pwd, cmd):
+    #print("Creating SSH Client..")
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    print("Connection..")
+    #print("Connection..")
     ssh_client.connect(hostname=ip, username=user, password=pwd)
-    print("Command execution..")
+    #print("Command execution..")
     ssh_client.exec_command(cmd)
     time.sleep(2)
     ssh_client.close()
