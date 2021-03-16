7365e3ce6f5f4f0c4a3ead13aedafcaf99940dd5
bodnar41 <b.maark97@gmail.com>
2021-03-16 13:06:42 +0100

    updated SSH gui

--- SSH_CONN_GUI.py
+++ SSH_CONN_GUI.py
@@ -1,17 +1,14 @@
 from tkinter import *
 import socket
-import sys
-from tkinter import filedialog, messagebox
-
+from tkinter import messagebox
 import paramiko, time
-from tkinter import ttk
 
 class SSH():
     def __init__(self, root):
         self.root = root
         self.root.title('SSH Teszt')
         self.root.geometry("600x400")
-        def ssh_conn(ip = None, user = None, pwd = None, cmd = None):
+        def ssh_conn(ip, user, pwd, cmd):
             if ip_input.index("end") != 0 and user_input.index("end") != 0 and pwd_input.index("end") != 0:
 
                 try:
@@ -27,25 +24,14 @@ class SSH():
                     stdout = stdout.readlines()
                     print(stdout)
                     ssh_client.close()
-                    # answer.config(text="")
-                    # answer.config(text="SSH connection is OK!")
                     messagebox.showinfo("Info", "SSH connection is OK!")
                 except paramiko.ssh_exception.AuthenticationException as e:
-                    # print("Username: " + user + "\t or password invalid.")
-                    # ssh_conn(ip_input.get(), None, None, cmd_input.get())
-                    # answer.config(text=f"Username:{user_input.get()} or password invalid!\nGive correct datas!")
                     messagebox.showerror("Error", f"Username:{user_input.get()} or password invalid!\nEnter correct data!")
 
-
                 except socket.gaierror:
-                    # print(ip + " invalid.")
-                    # ssh_conn(None, user_input.get(), pwd_input.get(), cmd_input.get())
-                    # answer.config(text=f"{ip_input.get()} invalid! Give correct address!")
                     messagebox.showerror("Error", f"{ip_input.get()} invalid! Enter a correct address!")
 
                 except:
-                    e = sys.exc_info()
-                    # answer.config(text=f"Unexpected error: {e}")
                     messagebox.showerror("Error", "Unexpected error: {e}")
             else:
                 messagebox.showwarning("Warning","IP Address, Username and Password are required!")
@@ -87,12 +73,6 @@ class SSH():
         testbtn.grid(row=5, column=1, padx=10, pady=10, sticky="w")
 
 
-        answer = Label(SSH_Frame, text=None, bg="grey", fg="white", font=("times new roman", 12))
-        answer.grid(row=6, column=1, padx=10)
-
-
-
 root = Tk()
 ob = SSH(root)
-
 root.mainloop()
